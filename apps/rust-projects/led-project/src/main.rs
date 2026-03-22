69882c57ea2939e0b8a076be23970fd9d10ea157
a.basile <antonio.basile@pnoinnovation.com>
2026-03-22 10:14:11 +0100

    feat: update main lain project

--- apps/rust-projects/led-project/src/main.rs
+++ apps/rust-projects/led-project/src/main.rs
@@ -7,17 +7,22 @@ fn main() -> anyhow::Result<()> {
     // Necessario per inizializzare l'ecosistema esp-idf
     esp_idf_svc::sys::link_patches();
 
+    esp_idf_svc::log::EspLogger::initialize_default();
+    log::info!("Starting LED project!");
+
     let peripherals = Peripherals::take()?;
 
     // Usa GPIO2 (LED built-in su molte schede ESP32)
     // Cambia il pin in base al tuo hardware
-    let mut led = PinDriver::output(peripherals.pins.gpio2)?;
+    let mut led = PinDriver::output(peripherals.pins.gpio26)?;
 
     loop {
         led.set_high()?;
+        log::info!("LED ON: I love cate");
         FreeRtos::delay_ms(1000);
-
+        
         led.set_low()?;
+        log::info!("LED OFF: I love cate");
         FreeRtos::delay_ms(1000);
     }
 }
