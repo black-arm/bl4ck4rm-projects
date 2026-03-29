0e0c43b4cb269f7dab382899484853a83c700401
black-arm <asant.b12@gmail.com>
2026-03-29 10:40:23 +0200

    fix: update articles query to include pagination and cache settings; refactor query client initialization in layout

--- apps/website/src/modules/blog/domain/endpoint.ts
+++ apps/website/src/modules/blog/domain/endpoint.ts
@@ -2,7 +2,7 @@ export class Endpoint {
   private static readonly DEV_TO_BASE_URL = 'https://dev.to/api';
   
   static getArticlesByUsername(username: string): string {
-    return `${this.DEV_TO_BASE_URL}/articles?username=${username}`;
+    return `${this.DEV_TO_BASE_URL}/articles?username=${username}&state=all`;
   }
   
   static get devToEndpoint(): string {
