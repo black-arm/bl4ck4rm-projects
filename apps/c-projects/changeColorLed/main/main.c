a9fd4b2f4c69a76a74b3ddc940922ed47ef8a63d
black-arm <asant.b12@gmail.com>
2026-04-19 19:24:19 +0200

    change color led projects created and vsc env setted

---
+++ apps/c-projects/changeColorLed/main/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "sdkconfig.h"
+#include <inttypes.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_chip_info.h"
+#include "esp_flash.h"
+#include "esp_system.h"
+
+void app_main(void)
+{
+}
