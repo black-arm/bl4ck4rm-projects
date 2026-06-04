ce7651d4a0c1d8fd0a7d442b193bf0d968d13ffc
black-arm <asant.b12@gmail.com>
2026-06-04 11:35:20 +0200

    feat projects created

---
+++ apps/c++-projects/c++-tut/1-first-project/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+
+    std::cout << "Hello World!" << std::endl;
+    std::cout << "My name is Antonio" << "\n"; // posso avere lo stesso risultato di std:endl
+
+    return 0;
+}
---
+++ apps/c++-projects/c++-tut/2-variables/variables.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+int main() {
+
+    int x = 10;
+    int y = 5;
+    int sum = x + y;
+
+    std::cout << x << '\n';
+    std::cout << y << '\n';
+    std::cout << sum << "\n";
+
+    //double (including decimal)
+    double price = 10.56;
+
+    std::cout << price << '\n';
+
+    //char (single character)
+    char a = 'a';
+    char b = 'b';
+    char endline = '\n';
+
+    std::cout << a << b << endline;
+
+    //boolean 
+    bool money = true; // 1
+
+    std::cout << money << '\n';
+
+    // string 
+    std::string name = "Antonio";
+
+    std::cout << name << '\n';
+    
+
+    return 0;
+}
