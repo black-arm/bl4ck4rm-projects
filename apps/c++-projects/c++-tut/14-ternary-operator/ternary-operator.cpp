3ae44fcbef62333fc0d0824b281bd5db852830d0
bl4ck4rm <asant.b12@gmail.com>
2026-06-11 17:21:57 +0200

    feat: add comments explaining the ternary operator usage

--- apps/c++-projects/c++-tut/14-ternary-operator/ternary-operator.cpp
+++ apps/c++-projects/c++-tut/14-ternary-operator/ternary-operator.cpp
@@ -3,7 +3,8 @@
 int main() {
 
     bool hungry = true;
-
+    // ternary operator: condition ? expression true : expression false
+    // replace if-else statement when assigning a value to a variable.
     hungry ? 
         std::cout << "I'm hungry!" << std::endl : 
         std::cout << "I'm not hungry." << std::endl;
